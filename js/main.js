e209523b0fc3ea2bd84df57cc06a766bcc70a4f9
tminhbao <tminhbao2001@gmail.com>
2022-09-30 17:59:03 +0700

    printBill of uberX

--- js/main.js
+++ js/main.js
@@ -63,3 +63,114 @@ function calcMoney() {
     spanTien.innerHTML = thanhTien;
   }
 }
+
+function printBill() {
+  var soKM = parseFloat(document.getElementById("soKM").value);
+  var thoiGianCho = parseFloat(document.getElementById("thoiGianCho").value);
+  var modalElement = document.getElementById("modalBodyContent");
+  var typeCar = getTypeCar();
+  switch (typeCar) {
+    case "uberX":
+      if (soKM <= 1) {
+        thanhTien = soKM * 8000 + thoiGianCho * 2000;
+        modalElement.innerHTML = `
+        <tr>
+            <td>uberX</td>
+            <td>1 km</td>
+            <td>8000</td>
+            <td>8000</td>
+        </tr>
+        <tr>
+            <td>Thời gian chờ</td>
+            <td>${thoiGianCho} phút</td>
+            <td>2000</td>
+            <td>${thoiGianCho * 2000}</td>
+        </tr>
+        <tr>
+            <td colspan="3">Total</td>
+            <td>${thanhTien}</td>
+        </tr>
+        `;
+      } else if (soKM > 1 && soKM <= 20) {
+        thanhTien = 8000 + (soKM - 1) * 12000 + thoiGianCho * 2000;
+        modalElement.innerHTML = `
+        <tr>
+            <td>uberX</td>
+            <td>1 km</td>
+            <td>8000</td>
+            <td>8000</td>
+        </tr>
+        <tr>
+            <td>uberX</td>
+            <td>19 km</td>
+            <td>12000</td>
+            <td>${19 * 12000}</td>
+        </tr>
+        <tr>
+            <td>Thời gian chờ</td>
+            <td>${thoiGianCho} phút</td>
+            <td>2000</td>
+            <td>${thoiGianCho * 2000}</td>
+        </tr>
+        <tr>
+            <td colspan="3">Total</td>
+            <td>${thanhTien}</td>
+        </tr>
+        `;
+      } else if (soKM > 20) {
+        thanhTien =
+          8000 + 20 * 12000 + (soKM - 21) * 10000 + thoiGianCho * 2000;
+        modalElement.innerHTML = `
+        <tr>
+            <td>uberX</td>
+            <td>1 km</td>
+            <td>8000</td>
+            <td>8000</td>
+        </tr>
+        <tr>
+            <td>uberX</td>
+            <td>19 km</td>
+            <td>12000</td>
+            <td>${19 * 12000}</td>
+        </tr>
+        <tr>
+            <td>uberX</td>
+            <td>${soKM - 20} km</td>
+            <td>10000</td>
+            <td>${(soKM - 20) * 10000}</td>
+        </tr>
+        <tr>
+            <td>Thời gian chờ</td>
+            <td>${thoiGianCho} phút</td>
+            <td>2000</td>
+            <td>${thoiGianCho * 2000}</td>
+        </tr>
+        <tr>
+            <td colspan="3">Total</td>
+            <td>${thanhTien}</td>
+        </tr>
+        `;
+      }
+      break;
+    case "uberSUV":
+      if (soKM <= 1) {
+        thanhTien = soKM * 9000 + thoiGianCho * 3000;
+      } else if (soKM > 1 && soKM <= 20) {
+        thanhTien = 9000 + (soKM - 1) * 14000 + thoiGianCho * 3000;
+      } else if (soKM > 20) {
+        thanhTien =
+          9000 + 20 * 14000 + (soKM - 21) * 10000 + thoiGianCho * 3000;
+      }
+      break;
+    case "uberBlack":
+      if (soKM <= 1) {
+        thanhTien = soKM * 10000 + thoiGianCho * 4000;
+      } else if (soKM > 1 && soKM <= 20) {
+        thanhTien = 10000 + (soKM - 1) * 16000 + thoiGianCho * 4000;
+      } else if (soKM > 20) {
+        thanhTien =
+          10000 + 20 * 16000 + (soKM - 21) * 14000 + thoiGianCho * 4000;
+      }
+      break;
+  }
+}
