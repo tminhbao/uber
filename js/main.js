e514f6aa523bee98d9d2ef880641ac80128ba3b0
tminhbao <tminhbao2001@gmail.com>
2022-10-06 17:02:52 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ function printBill() {
         <tr>
             <td>uberX</td>
             <td>${soKM} km</td>
-            <td>8000</td>
+            <td>${8000}</td>
             <td>${soKM * 8000}</td>
         </tr>
         <tr>
@@ -157,7 +157,7 @@ function printBill() {
         thanhTien = soKM * 9000 + thoiGianCho * 3000;
         modalElement.innerHTML = `
         <tr>
-            <td>uberX</td>
+            <td>uberSUV</td>
             <td>${soKM} km</td>
             <td>8000</td>
             <td>${soKM * 9000}</td>
@@ -177,13 +177,13 @@ function printBill() {
         thanhTien = 9000 + (soKM - 1) * 14000 + thoiGianCho * 3000;
         modalElement.innerHTML = `
         <tr>
-            <td>uberX</td>
+            <td>uberSUV</td>
             <td>1 km</td>
             <td>9000</td>
             <td>9000</td>
         </tr>
         <tr>
-            <td>uberX</td>
+            <td>uberSUV</td>
             <td>${soKM - 1} km</td>
             <td>12000</td>
             <td>${(soKM - 1) * 14000}</td>
@@ -204,19 +204,19 @@ function printBill() {
           9000 + 20 * 14000 + (soKM - 21) * 10000 + thoiGianCho * 3000;
         modalElement.innerHTML = `
           <tr>
-              <td>uberX</td>
+              <td>uberSUV</td>
               <td>1 km</td>
               <td>9000</td>
               <td>9000</td>
           </tr>
           <tr>
-              <td>uberX</td>
+              <td>uberSUV</td>
               <td>19 km</td>
               <td>14000</td>
               <td>${19 * 14000}</td>
           </tr>
           <tr>
-              <td>uberX</td>
+              <td>uberSUV</td>
               <td>${soKM - 20} km</td>
               <td>10000</td>
               <td>${(soKM - 20) * 10000}</td>
@@ -239,7 +239,7 @@ function printBill() {
         thanhTien = soKM * 10000 + thoiGianCho * 4000;
         modalElement.innerHTML = `
         <tr>
-            <td>uberX</td>
+            <td>uberBlack</td>
             <td>${soKM} km</td>
             <td>8000</td>
             <td>${soKM * 10000}</td>
@@ -259,13 +259,13 @@ function printBill() {
         thanhTien = 10000 + (soKM - 1) * 16000 + thoiGianCho * 4000;
         modalElement.innerHTML = `
         <tr>
-            <td>uberX</td>
+            <td>uberBlack</td>
             <td>1 km</td>
             <td>10000</td>
             <td>10000</td>
         </tr>
         <tr>
-            <td>uberX</td>
+            <td>uberBlack</td>
             <td>${soKM - 1} km</td>
             <td>12000</td>
             <td>${(soKM - 1) * 16000}</td>
@@ -286,19 +286,19 @@ function printBill() {
           10000 + 20 * 16000 + (soKM - 21) * 14000 + thoiGianCho * 4000;
         modalElement.innerHTML = `
         <tr>
-            <td>uberX</td>
+            <td>uberBlack</td>
             <td>1 km</td>
             <td>10000</td>
             <td>10000</td>
         </tr>
         <tr>
-            <td>uberX</td>
+            <td>uberBlack</td>
             <td>19 km</td>
             <td>12000</td>
             <td>${19 * 16000}</td>
         </tr>
         <tr>
-            <td>uberX</td>
+            <td>uberBlack</td>
             <td>${soKM - 20} km</td>
             <td>10000</td>
             <td>${(soKM - 20) * 14000}</td>
